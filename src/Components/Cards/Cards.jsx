5c8b5c35ac3a0cc0d2bae6d8e15dd96bf0d8180a
Gourav <gouravpaliwal196@gmail.com>
2024-12-16 17:21:26 +0530

    Adding expand Function

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -4,26 +4,31 @@ import { Button, Collapse, IconButton, Stack } from '@mui/material'
 import { KeyboardArrowDown } from '@mui/icons-material'
 
 const Cards = (props) => {
+    const [expand, setExpand] = useState(false);
+
+    const handleOnExpand = () => {
+        setExpand(!expand)
+    }
     return (
         <div>
             <div class="card" >
-                <img src={props.imgUrl} class="card-img-top" alt="..."/>
-                <Stack sx={{justifyContent: "space-between",padding: "0 5px 10px 5px"}}>
+                <img src={props.imgUrl} class="card-img-top" alt="..." />
+                <Stack sx={{ justifyContent: "space-between", padding: "0 5px 10px 5px" }}>
                     <div class="card-body">
                         <h5 class="card-title">{props.title}</h5>
-                        <Collapse in={props.expand}>
-                        <p class="card-text">{props.description}</p>
+                        <Collapse in={expand}>
+                            <p class="card-text">{props.description}</p>
                         </Collapse>
-                        <Stack sx={{justifyContent: "space-between", alignItems: "center"}} direction='row'>
-                        <span>{props.author} ~</span>
-                        <IconButton color='inherit' onClick={props.onExpand}>
-                            <KeyboardArrowDown/>
-                        </IconButton>
+                        <Stack sx={{ justifyContent: "space-between", alignItems: "center" }} direction='row'>
+                            <span>{props.author} ~</span>
+                            <IconButton color='inherit' onClick={handleOnExpand}>
+                                <KeyboardArrowDown />
+                            </IconButton>
                         </Stack>
                     </div>
-                        <Button disableRipple={true} variant='contained' color='success' href={props.url}>
-                            Learn more
-                        </Button>
+                    <Button disableRipple={true} variant='contained' color='success' href={props.url}>
+                        Learn more
+                    </Button>
                 </Stack>
             </div>
         </div>
